424dfb82a40750d38c7c69b70e1c6dd0e2b80ae4
Delia <deli.orellana@alumnos.duoc.cl>
2021-01-20 19:13:43 -0300

    clase producto

--- bola2/models.py
+++ bola2/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Producto(models.Model):
+    nombre = models.CharField(verbose_name="Nombre", max_length=50)
+    colores = models.CharField(verbose_name="Colores", max_length=50)
+    precio = models.IntegerField(verbose_name="Precio", max_length=50)
+    tamaño = models.IntegerField(verbose_name="Tamaño")
+    descripcion = models.CharField(verbose_name="Descripcion", max_length=50)
