c5881198438a4475f539a1df0a6930be8927b01c
Delia <deli.orellana@alumnos.duoc.cl>
2021-01-21 18:57:18 -0300

    modificacion clases

--- bola2/admin.py
+++ bola2/admin.py
@@ -1,28 +1,20 @@
 from django.contrib import admin
-<<<<<<< HEAD
-from bola2.models import Cliente, Producto, Galeria
-
-
-=======
-from .models import models
->>>>>>> 1ead3ebdbf2452d969121d1f744324523091294a
-# Register your models here.
-
+from bola2.models import Cliente, Producto, Galeria, Pedido
 class ClientesAdmin (admin.ModelAdmin):
     list_display=("nombre", "direccion", "Telefono")
     search_fields=("nombre", "Telefono")
 
 class ProductoAdmin (admin.ModelAdmin):
-    list_display=("producto", "color", "precio")
-    search_fields=("producto", "color")
+    list_display=("nombre", "colores", "precio")
+    search_fields=("nombre", "colores")
 
-class GaleriaAdmin (admin.ModelAdmin):
-    list_display=("nombre_imagen")
-
-
+class PedidoAdmin (admin.ModelAdmin):
+    list_display=("numero", "fecha")
+    list_filter=("numero", "fecha")
 
 
 admin.site.register(Cliente, ClientesAdmin)
-admin.site.register(Producto)
+admin.site.register(Producto, ProductoAdmin)
 admin.site.register(Galeria)
+admin.site.register(Pedido, PedidoAdmin)
 
