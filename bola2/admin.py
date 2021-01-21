b7ed87d2d9a70ee4088716dc643682c686f42abc
Delia <deli.orellana@alumnos.duoc.cl>
2021-01-21 17:59:59 -0300

    ingreso de registro

--- bola2/admin.py
+++ bola2/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from bola2.models import Cliente, Producto, Galeria
+
 
 # Register your models here.
+
+class ClientesAdmin (admin.ModelAdmin):
+    list_display=("nombre", "direccion", "Telefono")
+    search_fields=("nombre", "Telefono")
+
+class ProductoAdmin (admin.ModelAdmin):
+    list_display=("producto", "color", "precio")
+    search_fields=("producto", "color")
+
+class GaleriaAdmin (admin.ModelAdmin):
+    list_display=("nombre_imagen")
+
+
+
+
+admin.site.register(Cliente, ClientesAdmin)
+admin.site.register(Producto)
+admin.site.register(Galeria)
+
--- bola2/models.py
+++ bola2/models.py
@@ -2,9 +2,26 @@ from django.db import models
 
 # Create your models here.
 
+class Cliente(models.Model):
+    nombre=models.CharField(max_length=50)
+    direccion=models.CharField(max_length=50)
+    email=models.EmailField()
+    Telefono=models.CharField(max_length=9)
+
+    def __str__(self):
+        return self.nombre, self.direccion, self.email, self.Telefono
+
 class Producto(models.Model):
     nombre = models.CharField(verbose_name="Nombre", max_length=50)
     colores = models.CharField(verbose_name="Colores", max_length=50)
-    precio = models.IntegerField(verbose_name="Precio", max_length=50)
+    precio = models.IntegerField(verbose_name="Precio")
     tamaño = models.IntegerField(verbose_name="Tamaño")
-    descripcion = models.CharField(verbose_name="Descripcion", max_length=50)
+    descripcion = models.CharField(verbose_name="Descripcion", max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Galeria(models.Model):
+    nombre_imagen = models.CharField(verbose_name="Nombre Imagen", max_length=50)
+    descripcion = models.CharField(verbose_name="Descripcion", max_length=100)
