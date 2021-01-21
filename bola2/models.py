586d94d38593ecca704f5636f9eb1c99aed60376
wenchuankun <c.huenchuan@alumnos.duoc.cl>
2021-01-21 19:42:38 -0300

    actualizacion

--- bola2/models.py
+++ bola2/models.py
@@ -1,25 +1,34 @@
 from django.db import models
 
 # Create your models here.
-<<<<<<< HEAD
+class Cliente(models.Model):
+    nombre=models.CharField(max_length=50)
+    direccion=models.CharField(max_length=50)
+    email=models.EmailField()
+    Telefono=models.CharField(max_length=9)
+
+    def _str_(self):
+        return self.nombre, self.direccion, self.email, self.Telefono
+
 class Producto(models.Model):
     nombre = models.CharField(verbose_name="Nombre", max_length=50)
-    tamaño = models.CharField(verbose_name="Tamaño", max_length=20)
-    COLOR = [('1','Negro'),('2','Azul'),('3','Rojo'),('4','Verde'),('5','Blanco'),('6','Rosado')]
-    color = models.CharField(verbose_name="Color", choices=COLOR, max_length=1)
-    MATERIAL = [('1','Plastico'),('2','Vidrio'),('3','Metal')]
-    material = models.CharField(verbose_name="Material", choices=MATERIAL, max_length=1)
-    precio = models.IntegerField(verbose_name="Precio", max_length=50)
+    colores = models.CharField(verbose_name="Colores", max_length=50)
+    precio = models.IntegerField(verbose_name="Precio")
+    tamaño = models.IntegerField(verbose_name="Tamaño")
+    descripcion = models.CharField(verbose_name="Descripcion", max_length=100)
 
-    def __str__(self):
-        return self.nombre
-    
-class Descripcion(models.Model):
-    producto = models.ForeignKey(Producto, on_delete=models.DO_NOTHING, verbose_name="Producto")
-    reseña = models.TextField(verbose_name="Reseña")
-    
-    def __str__(self):
-        return self.producto
+    def _str_(self):
+        return self.nombre, self.colores, self.precio
 
 
-    
+class Galeria(models.Model):
+    nimagen = models.CharField(verbose_name="Nombre Imagen", max_length=50)
+    descripcion = models.CharField(verbose_name="Descripcion", max_length=100)
+
+    def _str_(self):
+        return self.nombre_imagen
+
+class Pedido(models.Model):
+    numero=models.IntegerField()
+    fecha=models.DateField()
+    entregado=models.BooleanField()
